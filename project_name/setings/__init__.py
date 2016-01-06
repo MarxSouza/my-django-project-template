ab1f1d67969c5d6ef86432553d4c7535285f05ab
MarxSouza <karlmarx12crvg@gmail.com>
2016-01-06 13:42:09 -0300

    second commit

--- project_name/setings/__init__.py
+++ project_name/setings/__init__.py
@@ -104,4 +104,3 @@ if DEBUG:
 # Production
 else:
     from settings.production import *
-    
--- project_name/setings/production.py
+++ project_name/setings/production.py
@@ -2,9 +2,12 @@ from .settings import *
 
 INSTALLED_APPS += ('compressor',)
 ALLOWED_HOSTS += ['127.0.0.1']
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': '{{ project_name }}',
+        'USER':'', 'PASS':'',
+        'HOST':'', 'PORT':'3306',
     }
 }
